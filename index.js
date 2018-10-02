ba605b68add20fd06bca7396f1cc5f244528f24b
BradKreager <bradley.kreager@sjsu.edu>
2018-10-02 16:16:57 -0700

    updates

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var express = require("express");
-//var jquery = require('jquery');
+var jquery = require('jquery');
 //var jquery = require('./node_modules/jquery/dist/jquery.js');
 //var popper = require("popper.js");
 //var bootstrap = require('./node_modules/bootstrap/dist/js/bootstrap.min.js');
+var bootstrap = require('bootstrap');
 var app = express();
 var router = express.Router();
 var path = __dirname + '/views/';
