a20e187c3b95754c3a12a3b4bc29060324578513
BradKreager <bradley.kreager@sjsu.edu>
2018-10-02 14:29:30 -0700

    updates

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-const http = require('http');
-const PORT = process.env.PORT || 5000;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+var express = require("express");
+//var jquery = require('jquery');
+var jquery = require('./node_modules/jquery/dist/jquery.js');
+//var popper = require("popper.js");
+var bootstrap = require('./node_modules/bootstrap/dist/js/bootstrap.min.js');
+var app = express();
+var router = express.Router();
+var path = __dirname + '/views/';
+
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
 });
-server.listen(PORT, () => {
-  console.log(`Server running on ${PORT}/`);
+
+router.get("/",function(req,res){
+  res.sendFile(path + "index.html");
 });
+
+
+app.use("/",router);
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use("*",function(req,res){
+  res.sendFile(path + "404.html");
+});
+
+app.listen(3000,function(){
+  console.log("Live at Port 3000");
+});
+
+
