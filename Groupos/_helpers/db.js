f31a0bd095907c81580a10d355e1beb31ee32bf7
Miguel Gonzalez <miguelg71921@gmail.com>
2018-10-16 13:41:16 -0700

    latest update

---
+++ Groupos/_helpers/db.js
@@ -0,0 +1,15 @@
+const config = require('config.json');
+const mongoose = require('mongoose');
+mongoose.connect(config.connectionString, {useNewUrlParser : true});
+mongoose.Promise = global.Promise;
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+	// we're connected!
+	console.log("Connected successfully to mongodb server");
+});
+
+module.exports = {
+	User: require('../users/user.model')
+};
---
+++ Groupos/index.js
@@ -0,0 +1,38 @@
+require('rootpath')();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const jwt = require('_helpers/jwt');
+const errorHandler = require('_helpers/error-handler');
+
+var router = express.Router();
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// use JWT auth to secure the api
+//app.use('/users', jwt());
+app.use('/users', jwt());
+
+// api routes
+app.use('/users', require('./users/users.controller'));
+
+// global error handler
+app.use(errorHandler);
+
+
+app.use(express.static(__dirname + '/dist/groupos-app'));
+app.use(express.static(__dirname));
+
+app.use("*",function(req,res){
+	res.sendFile(__dirname + "/dist/groupos-app/index.html");
+});
+
+app.listen(PORT,function(){
+	console.log(`Live at Port ${PORT}`);
+});
+
+
