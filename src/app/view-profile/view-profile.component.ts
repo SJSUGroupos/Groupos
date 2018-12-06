37836f0f2676bab3bbf0e0569b727c014ee649c5
BradKreager <bradley.kreager@sjsu.edu>
2018-12-06 13:47:54 -0800

    updates

--- src/app/view-profile/view-profile.component.ts
+++ src/app/view-profile/view-profile.component.ts
@@ -1,117 +1,124 @@
 import { Component, OnInit } from '@angular/core';
 import { first } from 'rxjs/operators';
-import { AlertService } from '../_services';
-import { Location } from '@angular/common';
-import { ActivatedRoute, Params, ParamMap, Router } from '@angular/router';
+import { Params, ActivatedRoute } from '@angular/router';
 import { User } from '../_models';
 import { UserService } from '../_services';
-import { Ng2ImgMaxService } from 'ng2-img-max';
+import { AlertService, EventService } from '../_services';
+import { Event } from '../_models';
+import { Event_Subscriber } from '../_models';
+import { Location } from '@angular/common';
 import { DecToTimePipe } from '../_pipes/dec-to-time.pipe';
-import * as $ from 'jquery';
-import * as moment from 'moment';
 
-const URL = 'users/avatar';
 
 @Component({
-	selector: 'app-view-profile',
-	templateUrl: './view-profile.component.html',
-	styleUrls: ['./view-profile.component.css']
+	selector: 'app-view-event',
+	templateUrl: './view-event.component.html',
+	styleUrls: ['./view-event.component.css']
 })
+export class ViewEventComponent implements OnInit {
 
-export class ViewProfileComponent implements OnInit {
-	selectedUser: User;
-	userId: string;
-	firstName: string;
-	lastName: string;
-	// avatar: string;
-	avail: Object;
-	major: String;
-	coursework: [String];
+	event: Event;
+	currentUser: User;
+	currentSubscribers: Event_Subscriber[];
+	eventId: string;
 	loading = false;
-	email: string;
+	subscribed = false;
+	userIsCreator = false;
 
-	constructor(private router: ActivatedRoute,
-		private userService: UserService,
+	constructor(private userService: UserService,
+		private eventService: EventService,
 		private alertService: AlertService,
 		private location: Location,
-		private ng2ImgMax: Ng2ImgMaxService) {
+		private route: ActivatedRoute) {
+		this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+		//alert(JSON.stringify(this.currentUser));
 	}
 
 	ngOnInit() {
 		this.loading = true;
-		this.userId = this.router.snapshot.params['id'];
-		this.loadUserData(() => {});
+		this.eventId = this.route.snapshot.params['id'];
+		this.loadEventData();
 	}
 
-	private loadUserData(cb?: () => void) {
-		this.userService.getById(this.userId).pipe(first()).subscribe(data => {
-			localStorage.setItem('selectedUser', JSON.stringify(data));
-			this.selectedUser = JSON.parse(localStorage.getItem('selectedUser'));
-			this.firstName = this.selectedUser.firstName;
-			this.lastName = this.selectedUser.lastName;
-			this.email = this.selectedUser.email;
-			this.major = this.selectedUser.major;
-			this.coursework = this.selectedUser.coursework;
-			this.avail = this.selectedUser.availabilities;
-			cb();
-		},
-		error => {
-			this.alertService.error(error); });
+	goBack(): void {
+		this.location.back();
+	}
+
+	subscribe() {
+		if(this.currentUser._id == this.event.creator['id']) {
+			return;
 		}
 
-		goBack(): void {
-			this.location.back();
-		}
+		let newSubscriber = new Event_Subscriber;
 
-	// retRange(start: number, end: number, zeroExtend: boolean) {
-	// 	var range = [];
-	// 	for (var i = start; i <= end; i++) {
-	// 		if(zeroExtend && i < 10) {
-	// 			range.push('0'+i.toString());
-	// 		}
-	// 		else {
-	// 			range.push(i.toString());
-	// 		}
-	// 	}
-	// 	return range;
-	// }
+		newSubscriber.id = this.currentUser._id;
+		newSubscriber.avatar = this.currentUser.avatar;
+		newSubscriber.email = this.currentUser.email;
+		newSubscriber.firstName = this.currentUser.firstName;
+		newSubscriber.lastName = this.currentUser.lastName;
+		newSubscriber.major = this.currentUser.major;
 
-// 	onFileChange(event) {
-// 		const reader = new FileReader();
-// 		var resizedImg = new Blob();
-//
-// 		if(event.target.files && event.target.files.length) {
-// 			const [file] = event.target.files;
-// 			this.ng2ImgMax.resizeImage(file, 100, 10000).subscribe(
-// 				result => {
-// 					//resizedImg = new File([result], result.name);
-// 					resizedImg = (result);
-// 					reader.readAsDataURL(resizedImg);
-// 					reader.onload = () => {
-// 						this.selectedUser.avatar = reader.result;
-// 						this.updateAvatarForm.patchValue({
-// 							avatar: reader.result
-// 						});
-//
-// 						// need to run CD since file load runs outside of zone
-// 						this.cd.markForCheck();
-// 					};
-// 				},
-// 				error => {
-// 					console.log('ng2ImgMax: ', error);
-// 				}
-// 			);
-//
-// 		}
-// 	}
-//
-// 	startTimeChange(startTimeH: string, startTimeM: string, periodS: string) {
-// 		var st = new Date('1/1/1970 '+ moment(startTimeH+':'+startTimeM+' '+periodS, ["h:mm A"]).format("HH:mm") +':00');
-// 		this.availForm.startTime.setValue(st);
-// 	}
-//
-// 	endTimeChange(endTimeH, endTimeM, period) {
-// 		var et = new Date('1/1/1970 '+ moment(endTimeH+':'+endTimeM+' '+period, ["h:mm A"]).format("HH:mm") +':00');
-// 		this.availForm.endTime.setValue(et);
-// 	}
- }
+		this.eventService.subscribe(this.event._id, newSubscriber)
+			.pipe(first())
+			.subscribe(
+				data => {
+					this.loadEventData();
+					this.alertService.success('Subscribed!');
+					//this.router.navigate(['/']);
+				},
+				error => {
+					this.alertService.error(error);
+					//this.loading = false;
+				});
+
+	}
+
+	unsubscribe() {
+
+		this.eventService.unsubscribe(this.event._id, { id: this.currentUser._id })
+			.pipe(first())
+			.subscribe(
+				data => {
+					this.loadEventData();
+					this.alertService.success('Unsubscribed!');
+				},
+				error => {
+					this.alertService.error(error);
+					//this.loading = false;
+				});
+	}
+
+
+	loadEventData(cb?: () => void) {
+		this.eventService.getById(this.eventId).pipe(first()).subscribe(event => {
+			this.event = event;
+			this.currentSubscribers = this.event.subscribers;
+			this.subscribed = this.isSubscribed();
+			this.userIsCreator = this.isCreator();
+			this.loading = false;
+		});
+	}
+
+	private isSubscribed() {
+		return this.currentSubscribers.find(o => o['id'] === this.currentUser._id) ? true : false;
+	}
+
+	isCreator() {
+		//return this.currentUser._id == input ? true : false;
+		return this.currentUser._id == this.event.creator['id'] ? true : false;
+
+	}
+
+	deleteEvent(eventId: string) {
+		this.loading = true;
+		this.eventService.delete(eventId)
+			.pipe(first())
+			.subscribe(
+				data => {
+					//this.loadEventData(() => { this.alertService.success('Event Deleted'); this.loading = false;  });
+					this.goBack();
+				});
+
+	}
+
+}
